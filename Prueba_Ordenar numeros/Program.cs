f61b92dc58f127c8055762626bbd157cfa00059e
Jose14Andres <167485194+Jose14Andres@users.noreply.github.com>
2024-06-18 15:03:29 -0500

    Agregar archivos de proyecto.

---
+++ Prueba_Ordenar numeros/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // arreglo  para almacenar los números ingresados
+        int[] numeros = new int[10];
+
+       
+        Console.WriteLine("Ingrese 10 números:");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Número {i + 1}: ");
+            numeros[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        for (int i = 0; i < numeros.Length - 1; i++)
+        {
+            for (int j = 0; j < numeros.Length - 1 - i; j++)
+            {
+                if (numeros[j] > numeros[j + 1])
+                {
+                    
+                    int temp = numeros[j];
+                    numeros[j] = numeros[j + 1];
+                    numeros[j + 1] = temp;
+                }
+            }
+        }
+
+        // Mostramos los números ordenados
+        Console.WriteLine("\nNúmeros ordenados:");
+
+        foreach (int numero in numeros)
+        {
+            Console.Write(numero + " ");
+        }
+
+        Console.WriteLine("\nPresione cualquier tecla para salir.");
+        Console.ReadKey();
+    }
+}
+
